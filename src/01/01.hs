f89ad2b8a950ba4f8b7920e4ef72b2cfc433ea7f
Leonardo Santos <leonardo.santos@klarna.com>
2021-12-01 13:29:37 +0100

    01: part 2

--- src/01/01.hs
+++ src/01/01.hs
@@ -4,6 +4,7 @@ main = do
   contents <- getContents
   let xs = numbers contents
   print $ solve1 xs
+  print $ solve2 xs
 
 numbers :: String -> [Int]
 numbers input =
@@ -17,3 +18,16 @@ solve1 xs =
    in count
   where
     f (previous, count) x = if x > previous then (x, count + 1) else (x, count)
+
+solve2 :: [Int] -> Int
+solve2 xs =
+  let queue = reverse $ take 3 xs
+      (_, count) = foldl f (queue, 0) (drop 3 xs)
+   in count
+  where
+    newQueue queue x = take 3 (x : queue)
+    f (previous, count) x =
+      let newQ = newQueue previous x
+       in if (sum newQ) > (sum previous)
+            then (newQ, count + 1)
+            else (newQ, count)
