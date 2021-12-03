80cd0819a8c9e4a5027660570362f3026dd030be
Leonardo Santos <leonardo.santos@klarna.com>
2021-12-03 18:56:19 +0100

    03: part 1

---
+++ src/03/03.hs
@@ -0,0 +1,39 @@
+-- https://adventofcode.com/2021/day/3
+
+import Data.Char (digitToInt)
+import Data.List (foldl')
+import qualified Data.Map as Map
+
+main = do
+  contents <- getContents
+  let parsedLines = fmap parse (lines contents)
+  let size = length $ head parsedLines
+  let (g, e) = solve1 parsedLines size
+  print (g * e)
+
+parse :: String -> [(Int, Char)]
+parse = zip [0 ..]
+
+toDec :: String -> Int
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+-- solve1 :: [[(Int, Char)]] -> Int ->  (Int, Int)
+solve1 lines size =
+  let hash = foldl f Map.empty lines
+      gamma = foldl g "" [0 .. size -1]
+      g :: [Char] -> Int -> [Char]
+      g acc x =
+        let c0 = hash Map.! (x, '0')
+            c1 = hash Map.! (x, '1')
+         in if c0 > c1 then acc ++ "0" else acc ++ "1"
+      epsilon = fmap (\x -> if x == '0' then '1' else '0') gamma
+      gammaDec = toDec gamma
+      epsilonDec = toDec epsilon
+   in (gammaDec, epsilonDec)
+  where
+    f :: Map.Map (Int, Char) Int -> [(Int, Char)] -> Map.Map (Int, Char) Int
+    f h line = foldl f' h line
+    f' :: Map.Map (Int, Char) Int -> (Int, Char) -> Map.Map (Int, Char) Int
+    f' h x = Map.insertWithKey f'' x 1 h
+    f'' :: (Int, Char) -> Int -> Int -> Int
+    f'' k v1 v2 = v1 + v2
